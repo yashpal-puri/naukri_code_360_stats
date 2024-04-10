00ddf85277078a1a1e0f28c6e824fc73a6a7bae2
yp8866 <122187543+yp8866@users.noreply.github.com>
2024-04-10 20:30:08 +0530

    added docker file and done configuration

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import puppeteer from 'puppeteer';
 import generateStats from './generate_svg.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './.env'
+})
 
 const app = express();
 
 app.get('/', async (req, res) => {    
         const username=req.query.username;
         const url = "https://www.naukri.com/code360/profile/"+username;
+        let svg;
         if (!username) {
-            const svg = generateStats({
+            svg = generateStats({
                 error: `Please include your profile username in the url`,
             });                
             res.setHeader("Content-Type", "image/svg+xml");
@@ -16,10 +22,19 @@ app.get('/', async (req, res) => {
         }
         
         (async () => {
+            let browser;
             try {                
-                const browser = await puppeteer.launch();
+                browser = await puppeteer.launch({
+                    args: [
+                        "--disable-setuid-sandbox",
+                        "--no-sandbox",
+                        "--single-process",
+                        "--no-zygote",
+                    ],
+                    executablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+                });
                 const page = await browser.newPage();
-                page.setDefaultNavigationTimeout(10 * 60 * 1000)
+                page.setDefaultNavigationTimeout(10 * 60 * 1000);
                 await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
                 await page.goto(url);
                 const searchResultSelector = '.problems-solved';
@@ -29,7 +44,6 @@ app.get('/', async (req, res) => {
                 );
                 const data = await textSelector?.evaluate(el => el.textContent);
                 const values= data.match(/\d+/g).map(Number);                
-                await browser.close();
                 const scrapedData = {
                     error: "",
                     username,
@@ -39,15 +53,17 @@ app.get('/', async (req, res) => {
                     hard: values[3],
                     ninja: values[4],
                 };
-                const svg = generateStats(scrapedData);                
-                res.setHeader("Content-Type", "image/svg+xml");
-                return res.send(svg);                
+                svg = generateStats(scrapedData);              
+                               
             } catch (error) {
-                const svg = generateStats({
+                svg = generateStats({
                     error: `Error fetching data for ${username}: ${error.message}`,
-                });   
+                });                           
+            }
+            finally{
+                await browser.close();
                 res.setHeader("Content-Type", "image/svg+xml");
-                return res.send(svg);                
+                return res.send(svg); 
             }
           })();   
 });
